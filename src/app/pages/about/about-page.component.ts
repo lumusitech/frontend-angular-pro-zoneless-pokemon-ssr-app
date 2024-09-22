11e83e45b27f7fd0b46885bca110d8d2eac42daf
Luciano Figueroa <lumusitech@gmail.com>
2024-09-22 17:32:27 -0300

    feat: add management for server side code and client side code

--- src/app/pages/about/about-page.component.ts
+++ src/app/pages/about/about-page.component.ts
@@ -1,8 +1,10 @@
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   OnInit,
+  PLATFORM_ID,
 } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
@@ -16,8 +18,19 @@ import { Meta, Title } from '@angular/platform-browser';
 export default class AboutPageComponent implements OnInit {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    if (isPlatformBrowser(this.platform)) {
+      //do something only for the client side
+      document.title = 'About page';
+    }
+
+    if (isPlatformServer(this.platform)) {
+      //do something only for the server side
+    }
+
+    //? Better
     this.title.setTitle('About page');
 
     this.meta.updateTag({
--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -11,17 +17,25 @@ import { Meta, Title } from '@angular/platform-browser';
 export default class PricingPageComponent {
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
-    this.title.setTitle('Pricing page');
+    if (isPlatformBrowser(this.platform)) {
+      //do something only for the client side
+      document.title = 'Pricing page';
+    }
 
+    if (isPlatformServer(this.platform)) {
+      //do something only for the server side
+    }
+
+    //? Better
+    this.title.setTitle('Pricing page');
     this.meta.updateTag({
       name: 'description',
       content: 'This is my pricing page',
     });
-
     this.meta.updateTag({ name: 'og:title', content: 'Pricing page' });
-
     this.meta.updateTag({
       name: 'keywords',
       content: 'angular,project,ssr,zoneless,lumusitech',
