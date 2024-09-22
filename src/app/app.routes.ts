221a47854635d2974b67b4e4fe8ac81c9b63c7ee
Luciano Figueroa <lumusitech@gmail.com>
2024-09-22 11:02:45 -0300

    feat: add base pages and router config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about-page.component'),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact-page.component'),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pages/pricing/pricing-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: '/about',
+    // new optional way with function to inject services pre redirect to destination
+    // redirectTo: () => {
+    //   const authService = inject(AuthService);
+    //   if (authService.isAuthenticated()) {
+    //     return '/about';
+    //   }
+    //   return '/login';
+    // },
+    pathMatch: 'full',
+  },
+];
