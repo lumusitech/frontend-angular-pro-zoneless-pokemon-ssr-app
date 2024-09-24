d3d82a894bc1d48368949053687ee58793fa0f7e
Luciano Figueroa <lumusitech@gmail.com>
2024-09-24 18:22:07 -0300

    test: add tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,68 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { TestBed, type ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './share/components/navbar/navbar.component';
 
-describe('AppComponent', () => {
+describe(`AppComponent`, () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+  let component: AppComponent;
+
+  @Component({
+    selector: 'navbar',
+    standalone: true,
+    template: `<h1>Hello world</h1>`,
+  })
+  class NavbarComponentMock {}
+
   beforeEach(async () => {
+    // TestBed.overrideComponent(AppComponent, {
+    //   set: {
+    //     imports: [NavbarComponentMock],
+    //     // tells to angular that ignores all schemas like providers, directives, pipes
+    //     schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    //   },
+    // });
+
+    // better way
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+      // app component render the navbar child component and navbar use a router
+      // navbar has dependencies on router
+      // we don't test navbar here
+      // one way, to test navbar, is to provide an empty router
+      // providers: [provideRouter([])],
+    })
+      //Better: Replace NavbarComponent with NavbarComponentMock
+      .overrideComponent(AppComponent, {
+        add: {
+          imports: [NavbarComponentMock],
+        },
+        remove: {
+          imports: [NavbarComponent],
+        },
+      })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+
+    // delete if not needed. It Detects input data changes
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(compiled).toBeTruthy();
+    expect(component).toBeTruthy();
+    console.log(compiled);
   });
 
-  it(`should have the 'frontend-angular-zoneless-pokemon-ssr-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-angular-zoneless-pokemon-ssr-app');
-  });
+  it('should render the navbar and router-outlet', () => {
+    const navbar = compiled.querySelector('navbar');
+    const routerOutlet = compiled.querySelector('router-outlet');
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-angular-zoneless-pokemon-ssr-app');
+    expect(navbar).toBeTruthy();
+    expect(routerOutlet).toBeTruthy();
   });
 });
